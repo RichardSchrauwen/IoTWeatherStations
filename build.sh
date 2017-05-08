724bffc3b8c0c03518341b5255b3200b44487a3f
Robert Barlin <robert.barlin@gmail.com>
2017-05-08 09:53:18 +0200

    silence non important errors

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 echo 'Building project'
 mvn compile assembly:single
 if [ $? -ne 0 ]; then
-  echo 'Failed to build, exiting...'
+  echo 'Failed to build, exiting ...'
   exit 1
 fi
 echo 'Build complete'
 
 echo 'Cleaning up'
-rm appiot-owm.jar
+rm appiot-owm.jar > /dev/null 2>&1
 mv target/owmgateway-1.0-SNAPSHOT-jar-with-dependencies.jar appiot-owm.jar
 cp appiot-owm.jar docker/appiot-owm/
-rm -rf target
+rm -rf target > /dev/null 2>&1
 echo 'Done'
