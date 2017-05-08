c3ee56aac609955103b5c260b6fe7f7e2af7a6d7
Robert Barlin <robert.barlin@gmail.com>
2017-05-08 09:39:19 +0200

    configurable run script with example

--- docker/appiot-owm/run.sh
+++ docker/appiot-owm/run.sh
@@ -1,14 +1,32 @@
 if [ $# -eq 0 ]; then
-  echo 'Missing name of container'
+  echo 'Missing config file path'
   exit 1
 fi
 
-sudo docker rm -f "owm-appiot-$1"
+source $1
+
+if [ -z $APPIOT_CONTAINER_NAME ]; then
+  echo 'Missing container name'
+  exit 1
+fi
+
+if [[ -z $APPIOT_REGISTRATION_TICKET ]]; then
+  echo 'Missing registration ticket'
+  exit 1
+fi
+
+if [ -z $APPIOT_COUCHDB_URL ]; then
+  echo 'Missing CouchDB URL'
+  exit 1
+fi
+
+
+sudo docker rm -f "appiot-owm-$APPIOT_CONTAINER_NAME"
 sudo docker run \
-  -e APPIOT_REGISTRATION_TICKET='<REGISTRATION-TICKET>' \
-  -e APPIOT_COUCHDB_URL='<COUCHDB-URL>' \
-  -e APPIOT_COUCHDB_USER='<COUCHDB-USER>' \
-  -e APPIOT_COUCHDB_PASSWORD='<COUCHDB-PASSWORD' \
+  -e APPIOT_REGISTRATION_TICKET="$APPIOT_REGISTRATION_TICKET" \
+  -e APPIOT_COUCHDB_URL=$APPIOT_COUCHDB_URL \
+  -e APPIOT_COUCHDB_USER=$APPIOT_COUCHDB_USER \
+  -e APPIOT_COUCHDB_PASSWORD=$APPIOT_COUCHDB_PASSWORD \
   -d \
-  --name "owm-appiot-$1" appiot-owm
+  --name "appiot-owm-$APPIOT_CONTAINER_NAME" appiot-owm
 
